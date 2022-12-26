dbc433840ac5af7594ca0a9c8b9801f2cf47e423
Prateek <shukla.prateek.tftuf.com>
2022-12-26 22:09:53 +0530

    Initial Commit

---
+++ src/Component/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./style.module.css";
+export const Footer = () => {
+  return (
+    <div className={style.container}>
+      <p className={style.footerText}>
+        © Copyright 2017 - 2022 | Rubixe is a brand of THINK AHEAD INNOVATIONS
+        PVT. LTD. | All Rights Reserved
+      </p>
+    </div>
+  );
+};
---
+++ src/Pages/MiddleScreen.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import style from "../styles/style.module.css";
+export const MiddleScreen = () => {
+  return <div className={style.container}>TopScreen</div>;
+};
